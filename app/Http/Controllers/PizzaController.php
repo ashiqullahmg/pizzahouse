df8259ac78ceb6e4531e1931360d6b4968f4b234
ashiqullahmg <ashiqullahmg@gmail.com>
2021-05-02 15:35:58 +0500

    "done"

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -1,20 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Pizza;
 
 class PizzaController extends Controller
 {
     public function index(){
-    $pizzas = [
-        ['type' => 'Hawaiian', 'base' => 'cheesy crust'],
-        ['type' => 'volcano', 'base' => 'garlic crust'],
-        ['type' => 'veg supreme', 'base' => 'this & crispy'],
-    ];
-    $name = request('name');
-    return view('pizzas', ['pizzas' => $pizzas, 'name' => request('name'),
-    'age' => request('age')]);
+    
+    // $pizzas = Pizza::all();
+    // $pizzas = Pizza::orderBy('name', 'desc')->get();
+    $pizzas = Pizza::where('type', 'hawaiian')->get();
+    return view('pizzas', ['pizzas'=>$pizzas]);
     }
 
     public function show($id){
--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -3,25 +3,16 @@
 @section('content')
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
 
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                   <h1>Pizza House</h1>
+            <div class="">
+                <div class="flex justify-center">
+                   <h1 style="text-transform: uppercase;
+	border-bottom: 2px solid; font-weight: normal; font-size: 50px;">Pizza List</h1>
+                   
                 </div>
-             
-
-            <!-- @for($i = 0; $i < count($pizzas); $i++)
-                <p>{{$pizzas[$i]['type']}} - {{$pizzas[$i]['base']}}</p>
-            @endfor -->
-
-            <p>{{$name}}</p>
-            <p>{{$age}}</p>
-
+               
             @foreach($pizzas as $pizza)
                 <div>
-              {{$loop -> index}}  {{$pizza['type']}} -- {{$pizza['base']}}
-              @if($loop -> first)
-              <span> - first in the loop</span>
-              @endif
+                {{$pizza->name}} - {{$pizza->type}} - {{$pizza->base}}
                 </div>
             @endforeach
             </div>
